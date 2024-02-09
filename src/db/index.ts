fab397395962a255785154cc1e041eb0f7965534
Johannes Chorzempa <jchorzempa@posteo.org>
2024-02-09 08:42:37 +0100

    create mongodb config

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,2 +1,23 @@
-// This is db/index.ts
-export default {};
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const { MONGODB_URI } = process.env;
+
+if (!MONGODB_URI) {
+	throw new Error('MONGODB_URI is not defined in the environment variables.');
+}
+
+console.log('connecting to', MONGODB_URI);
+
+const connectToMongoDB = async (): Promise<void> => {
+	try {
+		await mongoose.connect(MONGODB_URI);
+		console.log('connected to MongoDB');
+	} catch (error) {
+		console.error('error connection to MongoDB:', error.message);
+	}
+};
+
+void connectToMongoDB();
