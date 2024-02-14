6826c305de18ff3afa4d6e769dc6a5536c33322a
Johannes Chorzempa <jchorzempa@posteo.org>
2024-02-14 20:43:26 +0100

    archive types

---
+++ src/types/backend.ts
@@ -0,0 +1,134 @@
+// /* eslint-disable no-shadow */
+
+// export enum BackendEnv {
+// 	NODE_TS = 'node-ts',
+// 	NODE_JS = 'node-js',
+// 	NODE_EXPRESS_TS = 'node-express-ts',
+// 	NODE_EXPRESS_JS = 'node-express-js',
+// }
+
+// export enum BackendDataLayer {
+// 	GRAPHQL_SERVER = 'graphql-server',
+// }
+
+// export enum ModuleType {
+// 	COMMONJS = 'commonjs',
+// 	MODULE = 'module',
+// }
+
+// export enum Database {
+// 	MONGODB = 'mongodb',
+// 	POSTGRES = 'postgres',
+// }
+
+// export enum CMS {
+// 	KEYSTONE = 'keystoneJS',
+// 	STRAPI = 'strapi',
+// }
+
+// enum ModuleType {
+// 	commonjs
+// 	module
+// }
+
+// enum FrontFrame {
+// 	react-ts
+// 	react-js
+// 	vanilla-js
+// 	next-js
+// }
+// enum BackendEnv {
+// 	node-ts
+// 	node-js
+// 	node-express-ts
+// 	node-express-js
+// }
+// enum BackendDataLayer {
+// 	graphql-server
+// }
+
+// enum Database {
+// 	mongodb
+// 	postgres
+// }
+
+// enum CMS {
+// 	keystoneJS
+// 	strapi
+// }
+
+// enum Packages {
+// 	jsonwebtoken
+// 	express
+// 	cors
+// 	bcryptjs
+// 	tsx
+// 	dotenv
+// 	@graphql-codegen/cli
+// 	@graphql-codegen/typescript
+// 	@graphql-codegen/typescript-resolvers
+// 	nodemon
+// 	tsup
+// 	@apollo/client
+// 	graphql
+// 	typescript
+// 	@types/node
+// 	mongoose
+// 	pg
+// 	@apollo/server
+// 	graphql-tag
+// 	@types/jsonwebtoken
+// 	@types/bcryptjs
+// 	@types/cors
+// 	@types/nodemon
+// 	@types/pg
+// 	@types/express
+// }
+
+// enum BackendEnv {
+// 	nodets
+// 	nodejs
+// 	nodeExpressTS
+// 	nodeExpressJS
+// }
+// enum BackendDataLayer {
+// 	graphqlServer
+// }
+
+// enum Database {
+// 	mongodb
+// 	postgres
+// }
+
+// enum CMS {
+// 	keystoneJS
+// 	strapi
+// }
+
+// enum Packages {
+// 	jsonwebtoken
+// 	express
+// 	cors
+// 	bcryptjs
+// 	tsx
+// 	dotenv
+// 	GRAPHQL_CODEGEN_CLI
+// 	GRAPHQL_CODEGEN_TYPESCRIPT
+// 	GRAPHQL_CODEGEN_TYPESCRIPT_RESOLVERS
+// 	nodemon
+// 	tsup
+// 	apolloClient
+// 	graphql
+// 	typescript
+// 	typesNode
+// 	mongoose
+// 	pg
+// 	apolloServer
+// 	graphql-tag
+// 	typesJsonwebtoken
+// 	typesBcryptjs
+// 	typesCors
+// 	typesNodemon
+// 	typesPg
+// 	typesExpress
+// }
---
+++ src/types/frontend.ts
@@ -0,0 +1,7 @@
+/* eslint-disable no-shadow */
+// export enum FrontFrame {
+// 	REACT_TS = 'react-ts',
+// 	REACT_JS = 'react-js',
+// 	VANILLA_JS = 'vanilla-js',
+// 	NEXT_JS = 'next-js',
+// }
