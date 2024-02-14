5bdef42b211a1b8e12ef3dc8345643c831f9ec37
Johannes Chorzempa <jchorzempa@posteo.org>
2024-02-14 20:41:24 +0100

    add resolvers createProject & installscripts

--- src/resolvers.ts
+++ src/resolvers.ts
@@ -1,8 +1,10 @@
+import { GraphQLError } from 'graphql';
+import { Project, Resolvers } from './types';
 // // This is resolvers.ts
-// import { GraphQLError } from 'graphql';
 // import jwt from 'jsonwebtoken';
 
-import { Resolvers } from './types';
+import ProjectModel from './models/Project';
+import { generateInstallCommands } from './scripts/installCommands';
 
 // testresolvers
 const books = [
@@ -18,6 +20,82 @@ const books = [
 const resolvers: Resolvers = {
 	Query: {
 		books: () => books,
+	},
+
+	Mutation: {
+		createProject: async (root, args): Promise<Project> => {
+			const { title, description, createdBy, frontend, backend } = args;
+
+			try {
+				const existingProject = await ProjectModel.findOne({ title });
+
+				if (existingProject) {
+					throw new GraphQLError(`Project with title ${title} already exists`);
+				}
+
+				const newProjectData = {
+					title,
+					description,
+					createdBy,
+					frontend: {
+						...frontend,
+					},
+					backend: {
+						...backend,
+					},
+				};
+
+				await new ProjectModel(newProjectData).save();
+
+				return newProjectData;
+			} catch (error: unknown) {
+				throw new GraphQLError(`Failed to create project:`, {
+					extensions: {
+						code: 'INTERNAL_SERVER_ERROR',
+						invalidArgs: args,
+						error,
+					},
+				});
+			}
+		},
+		addInstallScript: async (root, args: { _id: string }): Promise<Project> => {
+			try {
+				if (!args) {
+					throw new Error('Invalid project ID');
+				}
+
+				const projectToUpdate = await generateInstallCommands(args._id);
+				const {
+					installScripts,
+					frontend: { packages: frontendPackages },
+					backend: { packages: backendPackages },
+				} = projectToUpdate;
+
+				const updatedProject = await ProjectModel.findByIdAndUpdate(
+					args._id,
+					{
+						installScripts,
+						frontend: { packages: frontendPackages },
+						backend: { packages: backendPackages },
+					},
+					{ new: true }
+				);
+
+				const mutationReturn: Project = {
+					backend: updatedProject?.backend,
+					createdBy: updatedProject?.createdBy as string,
+					description: updatedProject?.description,
+					frontend: updatedProject?.frontend,
+					installScripts: updatedProject?.installScripts,
+					title: updatedProject?.title,
+				};
+
+				return mutationReturn;
+			} catch (error) {
+				console.error('Error adding install script:', error.message);
+				throw error;
+			}
+		},
 	},
 };
 
