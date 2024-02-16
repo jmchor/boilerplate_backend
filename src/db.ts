e8fc4983d40fd1fc4376e79613535d6bce602fd8
Johannes Chorzempa <jchorzempa@posteo.org>
2024-02-16 13:57:20 +0100

    Move db function

---
+++ src/db.ts
@@ -0,0 +1,21 @@
+import dotenv from 'dotenv';
+import mongoose from 'mongoose';
+
+dotenv.config(); // load .env file from the path relative to where the function is envoked --> src/index.ts
+
+const { MONGODB_URI } = process.env;
+
+if (!MONGODB_URI) {
+	throw new Error('MONGODB_URI is not defined in the environment variables.');
+}
+
+export const connectToMongoDB = async (): Promise<void> => {
+	try {
+		await mongoose.connect(MONGODB_URI);
+		console.log('connected to MongoDB');
+	} catch (error) {
+		console.error('error connection to MongoDB:', error.message);
+	}
+};
+
+void connectToMongoDB();
