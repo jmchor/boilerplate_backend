7780c22fe8b96c996bbc131aa6531d4aa810f492
Johannes Chorzempa <jchorzempa@posteo.org>
2024-02-21 18:02:30 +0100

    create external args types

---
+++ src/types/argTypes.ts
@@ -0,0 +1,60 @@
+import { BackendConfig, FrontendConfig } from '../types';
+
+interface BaseArgs {
+	_id?: string;
+}
+
+interface EditProjectArgs extends BaseArgs {
+	title: string;
+	description: string;
+	createdBy: string;
+}
+
+interface CreateProjectArgs extends EditProjectArgs {
+	frontend: FrontendConfig;
+	backend: BackendConfig;
+}
+
+interface ArticleArgs extends BaseArgs {
+	title: string;
+	text: string;
+	imageUrl: string;
+	externalLink: string;
+	createdBy: string;
+}
+
+interface DeleteDocument extends BaseArgs {
+	createdBy: string;
+}
+
+interface LinkArticleToProject extends BaseArgs {
+	projectId: string;
+}
+
+interface LoginInput {
+	email?: string;
+	username?: string;
+	password: string;
+}
+
+interface EditUserArgs extends BaseArgs {
+	username: string;
+	email: string;
+}
+
+interface UpdatePasswordArgs extends BaseArgs {
+	oldPassword: string;
+	newPassword: string;
+}
+
+export {
+	BaseArgs,
+	EditProjectArgs,
+	CreateProjectArgs,
+	ArticleArgs,
+	DeleteDocument,
+	LinkArticleToProject,
+	LoginInput,
+	EditUserArgs,
+	UpdatePasswordArgs,
+};
