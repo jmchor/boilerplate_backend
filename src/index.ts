e32fdba09235c7e5b78637a9e67384e6a058d101
Johannes Chorzempa <jchorzempa@posteo.org>
2024-02-28 17:50:11 +0100

    remove User type

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,6 @@ import express, { Request, Response } from 'express';
 import cookieParser from 'cookie-parser';
 import cors from 'cors';
 import jwt from 'jsonwebtoken';
-import { User } from './types';
 import { connectToMongoDB } from './db';
 import typeDefs from './schema';
 import resolvers from './resolvers';
